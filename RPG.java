f7a7021009271482ca79d7e5bbf898eb0edf94dc
Evmaster <Evman3000@gmail.com>
2014-09-24 19:27:06 -0500

    Build 9/24/14

---
+++ RPG.java
@@ -0,0 +1,21 @@
+/**
+ * @(#)RPG.java
+ *
+ *
+ * @author Evan Jones and Derek Johnson
+ * @version 1.00 2014/9/17
+ */
+ 
+import java.util.*;
+
+public class RPG
+{
+
+	public static void main(String[] args) throws InterruptedException
+	{
+		Title t = new Title();
+		
+	}
+                                             
+    
+}
